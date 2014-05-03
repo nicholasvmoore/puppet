d7ff60f1e32f442fddb6cc15f713361e31a1c582
nicholasvmoore <nicholasvmoore@gmail.com>
2014-05-03 10:26:19 -0700

    Added the list of packages needed to compile graylog for RedHat, Debian/Ubuntu will come later

--- rlgeex_greylog2/manifests/init.pp
+++ rlgeex_greylog2/manifests/init.pp
@@ -11,5 +11,12 @@
 # Sample Usage:
 #
 class rlgeex_greylog2 {
+  case $operatingsystem {
+    'CentOS', 'RedHat', 'Fedora': { $tools = [ "make", "wget", "java-1.7.0-openjdk", "openssl-devel", "libyaml-devel", "httpd", "git", "ImageMagick", "ImageMagick-devel", "libxml2-devel", "libxslt-devel", "gcc-c++", "curl-devel", "httpd-devel", "apr-devel", "apr-util-devel"] }
+    'Debian', 'Ubuntu': { $tools = [ "build-essential" ] }
+    default:  { $tools = undef}
+  }
+    
+  package { $tools: ensure => "installed" }
 
 }
