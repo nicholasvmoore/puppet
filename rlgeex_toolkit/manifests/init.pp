1b47e463c517d6b1565cddf56282ac9d743745f1
Nicholas Moore <nicholas@foreman.pdx4.rlgeex.net>
2014-07-14 17:28:16 -0400

    adding loop_devices to a module for testing

--- rlgeex_toolkit/manifests/init.pp
+++ rlgeex_toolkit/manifests/init.pp
@@ -11,7 +11,7 @@
 # Sample Usage:
 #
 class rlgeex_toolkit {
-  $tools = [ "zsh", "htop", "screen", "tmux", "vim-enhanced", "nano", "tcpdump", "sudo", "perl", "python", "bind-utils", "mlocate", "xfsprogs", "yum-plugin-priorities", "subversion", "nfs-utils", "at", "man", "net-snmp"]    
+  $tools = [ "zsh", "htop", "screen", "tmux", "vim-enhanced", "nano", "tcpdump", "sudo", "perl", "python", "bind-utils", "mlocate", "xfsprogs", "yum-plugin-priorities", "subversion", "nfs-utils", "at", "man", "net-snmp", "yum-plugin-security"]    
 
   package { $tools: ensure => "installed" }
 
