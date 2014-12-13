2508485317fabe35c963766185eeb0e9d1014bbc
Nicholas Moore <nicholasvmoore@gmail.com>
2014-12-13 00:38:14 -0800

    fixed another syntax issue

--- open-vm-tools/manifests/init.pp
+++ open-vm-tools/manifests/init.pp
@@ -3,13 +3,13 @@ class open-vm-tools {
     'Redhat', 'CentOS': {
       if $operatingsystemmajrelease >= 7 {
         package { "open-vm-tools": ensure => "installed" }
-        serivce { "vmtoolsd": ensure => "running", enable => "true" }
+        serivce { "vmtoolsd": ensure => "running", enable => "true", }
       }
     }
     'Fedora': {
       if $operatingsystemmajrelease >= 19 {
         package { "open-vm-tools": ensure => "installed" }
-        serivce { "vmtoolsd": ensure => "running", enable => "true" }
+        serivce { "vmtoolsd": ensure => "running", enable => "true", }
       }
     }
   }
