d3cc798882038e627d7a47fce072ddc28acf9a5b
Nicholas Moore <nicholasvmoore@gmail.com>
2014-12-13 00:33:26 -0800

    fixing line 4 syntax error

--- open-vm-tools/manifests/init.pp
+++ open-vm-tools/manifests/init.pp
@@ -1,13 +1,13 @@
 class open-vm-tools {
   case $operatingsystem {
     'Redhat', 'CentOS': {
-      if $operatingsystemmajrelease >= 7: {
+      if $operatingsystemmajrelease >= 7 {
         package { "open-vm-tools": ensure => "installed" }
         serivce { "vmtoolsd": ensure => "running", enable => "true" }
       }
     }
     'Fedora': {
-      if $operatingsystemmajrelease >= 19: {
+      if $operatingsystemmajrelease >= 19 {
         package { "open-vm-tools": ensure => "installed" }
         serivce { "vmtoolsd": ensure => "running", enable => "true" }
       }
