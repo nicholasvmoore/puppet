9649a1edc6a4818ec71b029e759d83ff2b1f8795
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-15 19:03:10 -0800

    i doubt this will work, lol

--- docker-couchpotato/manifests/init.pp
+++ docker-couchpotato/manifests/init.pp
@@ -2,4 +2,9 @@ class docker-couchpotato {
   include docker::image
   include docker::run
   include docker-io
+
+  docker::image { 'nicholasvmoore/docker-couchpotato':
+    ensure => 'present',
+  }
+
 }
