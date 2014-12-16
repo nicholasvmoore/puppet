983159735a2ee2039b3444c8ca06212e8e6c9eee
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-15 19:16:04 -0800

    remove the duplicate import statements

--- docker-couchpotato/manifests/init.pp
+++ docker-couchpotato/manifests/init.pp
@@ -1,8 +1,4 @@
 class docker-couchpotato {
-  include docker::image
-  include docker::run
-  include docker-io
-
   docker::image { 'nicholasvmoore/docker-couchpotato':
     ensure => 'present',
   }
