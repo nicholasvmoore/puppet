cfb83b679d45d410207ddbb1fa11eaa56c40ed19
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-15 20:38:26 -0800

    convert string to num, issues on ubuntu and centos 6...strange it worked on 7 and fedora just fine....

--- open-vm-tools/manifests/init.pp
+++ open-vm-tools/manifests/init.pp
@@ -15,19 +15,19 @@ class open-vm-tools {
   if $is_virtual {
     case $operatingsystem {
       'RedHat', 'CentOS': {
-        if $operatingsystemmajrelease >= 7 {
+        if str2num($operatingsystemmajrelease) >= 7 {
           package { "open-vm-tools": ensure => "installed" }
           service { "vmtoolsd": ensure => "running", enable => "true", }
         }
       }
       'Fedora': {
-        if $operatingsystemmajrelease >= 19 {
+        if str2num($operatingsystemmajrelease) >= 19 {
           package { 'open-vm-tools': ensure => 'installed', }
           service { 'vmtoolsd': ensure => 'running', enable => 'true', }
         }
       }
       'Ubuntu': {
-        if $operatingsystemmajrelease >= 10.04 {
+        if str2num($operatingsystemmajrelease) >= 10.04 {
           package { 'open-vm-tools': ensure => 'installed', }
           service { 'open-vm-tools': ensure => 'running', enable => 'true', }
         }
