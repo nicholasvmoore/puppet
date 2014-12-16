da04f571c96d76df5712a314cd5dc30de8db8ec1
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-15 20:40:29 -0800

    moving back, that didn't help

--- open-vm-tools/manifests/init.pp
+++ open-vm-tools/manifests/init.pp
@@ -15,19 +15,19 @@ class open-vm-tools {
   if $is_virtual {
     case $operatingsystem {
       'RedHat', 'CentOS': {
-        if str2num($operatingsystemmajrelease) >= 7 {
+        if $operatingsystemmajrelease >= 7 {
           package { "open-vm-tools": ensure => "installed" }
           service { "vmtoolsd": ensure => "running", enable => "true", }
         }
       }
       'Fedora': {
-        if str2num($operatingsystemmajrelease) >= 19 {
+        if $operatingsystemmajrelease >= 19 {
           package { 'open-vm-tools': ensure => 'installed', }
           service { 'vmtoolsd': ensure => 'running', enable => 'true', }
         }
       }
       'Ubuntu': {
-        if str2num($operatingsystemmajrelease) >= 10.04 {
+        if $operatingsystemmajrelease >= 10.04 {
           package { 'open-vm-tools': ensure => 'installed', }
           service { 'open-vm-tools': ensure => 'running', enable => 'true', }
         }
