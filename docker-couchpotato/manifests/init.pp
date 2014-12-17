7da1e5f3b9a2203663358d7ed4ea30bd049c2bc2
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-16 20:46:20 -0800

    changed expose to ports

--- docker-couchpotato/manifests/init.pp
+++ docker-couchpotato/manifests/init.pp
@@ -4,7 +4,7 @@ class docker-couchpotato {
   }
   docker::run { 'couchpotato':
     image     => 'nicholasvmoore/docker-couchpotato',
-    expose    => ['5050:5050'],
+    ports    => ['5050:5050'],
     volumes   => ['/mnt/lun0/media:/media:rw', '/mnt/lun0/software/docker/movies:/config:rw'],
   }
 }
