4a1c1a742c845efc2acc978488f65855ed51b0ff
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-16 20:31:11 -0800

    testing my first docker run

--- docker-couchpotato/manifests/init.pp
+++ docker-couchpotato/manifests/init.pp
@@ -2,5 +2,10 @@ class docker-couchpotato {
   docker::image { 'nicholasvmoore/docker-couchpotato':
     ensure => 'present',
   }
-
+  docker::run { 'couchpotato':
+    image     => 'docker-couchpotato',
+    ports     => ['5050'],
+    expose    => ['5050'],
+    volumes   => ['/mnt/lun0/media', '/media', '/mnt/lun0/software/docker/movies', 'config'],
+  }
 }
