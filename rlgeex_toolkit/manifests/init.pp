8dfe67291d764dcbaf8a78c8a52d31b87dbe8dbc
nicholasvmoore <nicholasvmoore@gmail.com>
2014-12-18 17:30:19 -0800

    added the removal of firewalld

--- rlgeex_toolkit/manifests/init.pp
+++ rlgeex_toolkit/manifests/init.pp
@@ -14,10 +14,12 @@ class rlgeex_toolkit {
   case $osfamily {
     'RedHat': {
       $tools = [ 'zsh', 'htop', 'screen', 'tmux', 'vim-enhanced', 'nano', 'tcpdump', 'sudo', 'perl', 'python', 'bind-utils', 'mlocate', 'xfsprogs', 'yum-plugin-priorities', 'subversion', 'nfs-utils', 'at', 'net-snmp']
+      $remove_tools = [ 'firewalld' ]
     }
     'Debian': {
       $tools = [ 'zsh', 'htop', 'bind9utils', 'screen', 'tmux', 'nano', 'tcpdump', 'sudo', 'perl', 'python', 'mlocate', 'xfsprogs', 'subversion', 'at', 'nfs-common', 'vim-puppet', 'vim-addon-manager', 'vim', 'vim-common', 'snmpd']
     }
   }
   package { $tools: ensure => 'installed' }
+  package { $remove_tools ensure => 'purged' }
 }
